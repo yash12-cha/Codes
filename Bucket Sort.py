43fca140886951677833800fc329f401bcc567ba
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2021-11-01 16:19:02 +0530

    Create Bucket Sort.py

---
+++ Bucket Sort.py
@@ -0,0 +1,26 @@
+def bucketSort(array):
+    bucket = []
+
+    # Create empty buckets
+    for i in range(len(array)):
+        bucket.append([])
+
+    # Insert elements into their respective buckets
+    for j in array:
+        index_b = int(10 * j)
+        bucket[index_b].append(j)
+
+    # Sort the elements of each bucket
+    for i in range(len(array)):
+        bucket[i] = sorted(bucket[i])
+    # Flattening the Array
+    k = 0
+    for i in range(len(array)):
+        for j in range(len(bucket[i])):
+            array[k] = bucket[i][j]
+            k += 1
+    return array
+N = int(input("Enter number of elements: "))
+Arr = list(map(float,input("Enter array elements: ").strip().split()))
+bucketSort(Arr)
+print("Sorted Array: ",*Arr)
