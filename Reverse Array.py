d7468ac1e37a800ade655514c55dee725417d1f8
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2021-11-15 20:33:43 +0530

    Update Reverse Array.py

--- Reverse Array.py
+++ Reverse Array.py
@@ -1,11 +1,9 @@
 N = int(input("Enter number of elements: "))
-Arr = list(map(int,input("Enter the elements: ").strip().split()))[:N]
+Arr = list(map(int,input("Enter elements: ").strip().split()))
 start = 0
 end = N - 1
-while start < end:
-    temp = Arr[start]
-    Arr[start] = Arr[end]
-    Arr[end] = temp
-    start = start + 1
-    end = end - 1
+while(start < end):
+    Arr[start],Arr[end] = Arr[end],Arr[start]
+    start += 1
+    end -= 1
 print("Reverse Array:",*Arr)
