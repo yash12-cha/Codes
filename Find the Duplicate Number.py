db3ae9841c880a0932adc9f97afabff0ae3c5f45
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2021-11-16 22:20:07 +0530

    Update Find the Duplicate Number.py

--- Find the Duplicate Number.py
+++ Find the Duplicate Number.py
@@ -1,16 +1,10 @@
-N = int(input("Enter number of elements: "))
-Arr = list(map(int,input("Enter the elements: ").strip().split()))[:N]
-for i in range(N):
-    min = i
-    for j in range(i+1,N):
-        if Arr[min] > Arr[j]:
-            min = j
-    # Swapping
-    temp= Arr[i]
-    Arr[i] = Arr[min]
-    Arr[min] = temp
-for i in range(0,N-1):
-    if Arr[i] == Arr[i+1]:
-        flag = Arr[i]
-        break
-print(flag)  
+def findDuplicate(self, nums):
+        freq = {}
+        for i in nums:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
+        for i in freq:
+            if freq[i] > 1:
+                return i
