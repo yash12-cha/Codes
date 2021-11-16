7c7feb3e0485b9f3dbcd74207e5deab51a30a402
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2021-11-16 21:34:59 +0530

    Create Kth largest element.py

---
+++ Kth largest element.py
@@ -0,0 +1,9 @@
+def kthLargest(self,arr, l, r, k):
+        '''
+        arr : given array
+        l : starting index of the array i.e 0
+        r : ending index of the array i.e size-1
+        k : find kth smallest element and return using this function
+        '''
+        arr.sort(reverse = True)
+        return arr[k-1]
