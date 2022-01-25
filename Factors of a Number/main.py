a454e75451f192855704911ed7e77736f1f0a33e
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-01-25 13:06:41 +0530

    Update main.py

--- Factors of a Number/main.py
+++ Factors of a Number/main.py
@@ -5,9 +5,10 @@ i = 1
 # This will loop from 1 to int(sqrt(x))
 while i <= int(sqrt(x)):
     if x % i == 0:     # Check if i divides x without leaving a remainder
-        result.append(i)
-        # Handle the case explained in the 4th (Refer Document)
-        if x//i != i:
-            result.append(x//i)
+        if i * i ==x:  # Handle the case explained in the 4th (Refer Document)
+                result.append(i)
+            else:
+                result.append(i)
+                result.append(x//i)
     i += 1
 print("Factors of the given number are: ",*result)
