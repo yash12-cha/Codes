b1a10169e1fa1e4162a6171b5040662d7ce5149c
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2023-10-19 16:00:42 +0530

    Delete Reverse a String_1.py

--- Reverse a String_1.py
+++
@@ -1,7 +0,0 @@
-def reverseWord(s):
-    str = ""
-    for i in s:
-        str = i + str
-    return str
-  
-  # In above code, we call a function to reverse a string, which iterates to every element and intelligently join each character in the beginning so as to obtain the reversed string.
